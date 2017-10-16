482805fb0f64ad16c63e697531d1d422a43be5ca
nkk36 <nkallfa36@gmail.com>
2017-10-15 22:55:07 -0400

    Minor changes to scrape images code

--- code/ScrapeImages.py
+++ code/ScrapeImages.py
@@ -7,7 +7,7 @@ Created on Sun Oct 15 12:52:26 2017
 
 import urllib
 
-i = 10877
-for url in Images.imlink[i:]:
+i = 20377
+for url in Images.imlink[20377:33323]:
     urllib.request.urlretrieve(url, "C:\\Users\\NKallfa\\Desktop\\Documents\\Georgetown Data Science Certificate\\DMV\\\data\\images\\"+str(i)+".jpg")
     i+=1
